32564c72043d434329ac4bb0e6f1ac716a2a787f
daneFram <dsears@hawaii.edu>
2022-04-16 11:55:17 -1000

    Initial commit

---
+++ Weight.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Dane Sears <dsears@hawaii.edu>
+/// @date   26_APR_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Weight.h"
