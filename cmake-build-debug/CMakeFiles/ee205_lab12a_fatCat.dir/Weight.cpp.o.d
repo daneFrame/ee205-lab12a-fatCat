4296ded9bc6f76d2d606e2214c7722252ccd01f7
daneFram <dsears@hawaii.edu>
2022-04-17 14:06:35 -1000

    fixed all noncmake related errors

---
+++ cmake-build-debug/CMakeFiles/ee205_lab12a_fatCat.dir/Weight.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/ee205_lab12a_fatCat.dir/Weight.cpp.o: \
+ /tmp/tmp.zXoVYjcLBw/Weight.cpp /usr/include/stdc-predef.h
---
+++ cmake-build-debug/CMakeFiles/ee205_lab12a_fatCat.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/ee205_lab12a_fatCat.dir/main.cpp.o: \
+ /tmp/tmp.zXoVYjcLBw/main.cpp /usr/include/stdc-predef.h
